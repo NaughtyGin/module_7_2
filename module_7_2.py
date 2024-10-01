f4110bcadee2272d28b8a3fac0c1405b4a6f89a2
NaughtyGin <as519155@gmail.com>
2024-10-01 11:00:34 +1000

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,15 +1,14 @@
 def custom_write(file_name, strings):
     strings_positions = {}
     num_string = 0
-    file = open(file_name, 'w', encoding='utf-8')
-    file.close()
-    if isinstance(strings, list):
-        for string in strings:
-            num_string += 1
-            file = open(file_name, 'a+', encoding='utf-8')
-            strings_positions[(num_string, file.tell())] = string
-            file.write(f'{string}\n')
-            file.close()
+    with open(file_name, 'w', encoding='utf-8') as file:  # если нужно "обнулять" файл при новом запуске кода
+    # with open(file_name, 'a+', encoding='utf-8') as file:  # если нужно сохранить в файле всю историю запусков кода
+        if isinstance(strings, list):
+            for string in strings:
+                num_string += 1
+                strings_positions[(num_string, file.tell())] = string
+                file.write(f'{string}\n')
+        # file.close()
     return strings_positions
 
 
